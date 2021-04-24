f04c7232c76be5a0c1876105717c93cee311ed65
kuduzovkhamzat@gmail.com <khamzat995@gmail.com>
2021-04-24 14:36:38 +0300

    solutions

---
+++ solutions.js
@@ -0,0 +1,64 @@
+let aaab = fetch('https://reqres.in/api/users')
+  .then(res => res.json())
+  .then((data) => {
+    console.log(data)
+    })
+
+
+let asss = fetch('https://this-site-doesnt-exists.intocode')
+  .then(res => res.json())
+  .then((data) => {
+    console.log(data)
+})
+  .catch (err => console.log(err))
+
+
+let aaad = fetch('https://reqres.in/api/users')
+  .then(res => res.json())
+  .then((data) => {
+    console.log(typeof data)
+  })
+
+
+let aaac = fetch('https://reqres.in/api/users/1')
+  .then(res => res.json())
+  .then((data) => {
+    console.log(data.data.email)
+  })
+
+let aaaz = fetch('https://reqres.in/api/users', {
+  method: 'POST',
+    body: `{ "first_name": "intocode" }`,
+    headers: {
+    'Content-type' : 'application/json'
+    }
+})
+  .then(res => res.json())
+  .then((data) => {
+    console.log(data)
+  })
+
+let aaat = fetch('https://reqres.in/api/users', {
+  method: 'POST',
+  body: JSON.stringify({ "first_name": "intocode" }),
+  headers: {
+    'Content-type' : 'application/json'
+  }
+})
+  .then(res => res.json())
+  .then((data) => {
+    console.log(data)
+  })
+
+///
+сonst aaart = fetch('https://reqres.in/api/users', {
+  method: 'POST',
+  body: JSON.stringify({ "first_name": "intocode" }),
+  headers: {
+    'Content-type' : 'application/json'
+  }
+})
+  .then(res => res.json())
+  .then((data) => {
+    console.log(data)
+  })
